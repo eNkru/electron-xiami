9df6f6f02fa93460e32758b9f33711f1573d8df9
Howard Ju <howard.ju@tvnz.co.nz>
2017-06-23 17:31:39 +1200

    implementing the local cache register the playlist update register the change song notification

--- src/window/controller/player.js
+++ src/window/controller/player.js
@@ -1,4 +1,11 @@
 const { BrowserWindow } = require('electron');
+const urlLib = require('url');
+const path = require('path');
+const fetch = require('electron-fetch');
+const storage = require('electron-json-storage');
+
+const playlistUrl = 'http://www.xiami.com/song/playlist';
+const getSongUrl = 'http://www.xiami.com/song/gethqsong';
 
 class XiamiPlayer {
     constructor() {
@@ -22,8 +29,10 @@ class XiamiPlayer {
             }
         });
 
+        // load xiami player page.
         this.playerWindow.loadURL(this.playerUrl);
 
+        // triggering when user try to close the play window.
         this.playerWindow.on('close', (e) => {
             if (this.playerWindow.isVisible()) {
                 e.preventDefault();
@@ -31,22 +40,63 @@ class XiamiPlayer {
             }
         });
 
+        // triggering after the play window closed.
         this.playerWindow.on('closed', () => {
             this.playerWindow = null;
         });
+
+        this.playerWindow.webContents.on('did-get-response-details', ((status, newURL, originalURL) => this.registerResponseFilters(originalURL)));
     }
 
+    // display and focus the player window.
     show() {
         this.playerWindow.show();
         this.playerWindow.focus();
     }
 
+    // hide the play window.
     hide() {
         this.playerWindow.hide();
     }
 
+    // return a boolean to indicate if the window is visible or not
     isVisible() {
         return this.playerWindow.isVisible();
+    }
+
+    registerResponseFilters(requestUrl) {
+        this.updatePlaylistListener(requestUrl);
+        this.changeTrackListener(requestUrl);
+    }
+
+    updatePlaylistListener(requestUrl) {
+        if (requestUrl.startsWith(playlistUrl)) {
+            let urlWithPath = urlLib.parse(requestUrl, false);
+            delete urlWithPath.search;
+            console.log('Retrieve the playlist from url ' + urlLib.format(urlWithPath));
+            fetch(urlLib.format(urlWithPath)).then(res => res.json()).then(json => {
+                // clear the local storage.
+                storage.clear((error) => {
+                    if (error) console.log(error);
+                });
+
+                // refresh the local storage.
+                json.data.trackList.forEach(track => {
+                    storage.set(track.songId, track, (error) => {
+                        if (error) console.log(error);
+                    });
+                });
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+    }
+
+    changeTrackListener(requestUrl) {
+        if (requestUrl.startsWith(getSongUrl)) {
+            let pathname = urlLib.parse(requestUrl).pathname;
+            console.log(path.parse(pathname).base);
+        }
     }
 }
 
