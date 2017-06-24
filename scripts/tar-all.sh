09194fa82ce478b21cd42bfcd1b795e5166354ad
Huawu Ju <enkru2000@hotmail.com>
2017-06-24 22:50:19 +1200

    change the tar script

--- scripts/tar-all.sh
+++ scripts/tar-all.sh
@@ -3,15 +3,15 @@
 cd dist
 
 echo 'Start compressing for Mac OS X.'
-tar zcf 'mac-osx.tar.gz' 'Electronic WeChat-darwin-x64'
+tar zcf 'mac-osx.tar.gz' 'Electron Xiami-darwin-x64'
 echo 'Compressing for Mac OS X succeed.'
 
 echo 'Start compressing for Linux x64.'
-tar zcf 'linux-x64.tar.gz' 'electronic-wechat-linux-x64'
+tar zcf 'linux-x64.tar.gz' 'electron-xiami-linux-x64'
 echo 'Compressing for Linux x64 succeed.'
 
 echo 'Start compressing for Linux ia32.'
-tar zcf 'linux-ia32.tar.gz' 'electronic-wechat-linux-ia32'
+tar zcf 'linux-ia32.tar.gz' 'electron-xiami-linux-ia32'
 echo 'Compressing for Linux ia32 succeed.'
 
 cd ..
