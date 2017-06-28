f8b5027eccd5cb304d8f5e34112a04acaeec6e0a
Howard Ju <howard.ju@tvnz.co.nz>
2017-06-28 19:03:18 +1200

    import jQuery manually

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,21 +4,23 @@ const settings = require('electron-settings');
 const language = settings.get('language', 'en');
 const Locale = language === 'en' ? require('../locale/locale_en') : require('../locale/locale_sc');
 
-setLocale();
-
-
-$('#language').dropdown('set selected', language);
-
-$('#language').dropdown({
-  onChange: (value) => {
-    settings.set('language', value);
-  }
+$(() => {
+  setLocale();
+  loadSettings();
 });
 
 function setLocale() {
-  $(() => {
-    document.title = Locale.SETTINGS_SETTINGS_TITLE;
-    $('#setting-header').text(Locale.SETTINGS_SETTINGS);
-    $('#language-title').text(Locale.SETTINGS_LANGUAGE);
+  document.title = Locale.SETTINGS_SETTINGS_TITLE;
+  $('#setting-header').text(Locale.SETTINGS_SETTINGS);
+  $('#language-title').text(Locale.SETTINGS_LANGUAGE);
+}
+
+function loadSettings() {
+  $('#language').dropdown('set selected', language);
+
+  $('#language').dropdown({
+    onChange: (value) => {
+      settings.set('language', value);
+    }
   });
 }
