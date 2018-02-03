0cff5bc0e54823ec2a5638005b9bf8ea73949e65
Huawu Ju <enkru2000@hotmail.com>
2018-02-03 21:58:55 +1300

    Disable the tray click function for macOS due to no left click

--- src/js/settings.js
+++ src/js/settings.js
@@ -33,17 +33,22 @@ function loadSettings() {
   });
 
   // tray click setting
-  const trayClick = settings.get('trayClickEvent', 'showMain');
-  const $trayClick = $('#tray-click');
-  $trayClick.dropdown('setup menu', {values: Locale.SETTINGS_TRAY_CLICK_OPTIONS});
-  $trayClick.dropdown('set selected', trayClick);
-
-  $trayClick.dropdown({
-    onChange: (value) => {
-      settings.set('trayClickEvent', value);
-      ipcRenderer.send('trayClickEvent', value);
-    }
-  });
+  
+  if (process.platform === 'darwin') {
+    $('#tray-segment').hide();
+  } else {
+    const trayClick = settings.get('trayClickEvent', 'showMain');
+    const $trayClick = $('#tray-click');
+    $trayClick.dropdown('setup menu', {values: Locale.SETTINGS_TRAY_CLICK_OPTIONS});
+    $trayClick.dropdown('set selected', trayClick);
+  
+    $trayClick.dropdown({
+      onChange: (value) => {
+        settings.set('trayClickEvent', value);
+        ipcRenderer.send('trayClickEvent', value);
+      }
+    });
+  }
 
   // show notification
   const showNotification = settings.get('showNotification', 'check');
